a3de12b8d27f93b4138e4f9d6f77f64da790b32d
Thomas Kottke <t.kottke90@gmail.com>
2025-06-29 14:24:41 -0500

    refactor: fix hover styles on navigation links

--- src/components/navigation-link.tsx
+++ src/components/navigation-link.tsx
@@ -13,7 +13,7 @@ export function NavigationLink({ label, icon: Icon, href, active }: Readonly<{
   const pathname = usePathname()
 
   const baseClasses = "block p-2 flex  justify-center md:justify-start items-center gap-2";
-  const activeClasses = pathname.startsWith(href) || active ? "bg-illuminating-emerald-100 text-illuminating-emerald-800" : "hover:bg-gray-200";
+  const activeClasses = pathname.startsWith(href) || active ? "bg-illuminating-emerald-200 text-illuminating-emerald-800" : "hover:bg-zinc-300 dark:hover:bg-zinc-700";
 
   return (<Link className={`${baseClasses} ${activeClasses}`} href={href}>
     <Icon className="w-8 h-8 md:w-5 md:h-5" />
